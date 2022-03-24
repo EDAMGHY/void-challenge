ec98287a3671d5e00811fc465807e5f28de83ea2
Abdellah DAMRI <damriabdellah@gmail.com>
2022-03-24 18:00:18 +0100

    Adding Loading component

--- src/components/List.js
+++ src/components/List.js
@@ -3,21 +3,31 @@ import Item from './Item';
 const List = ({ show, setShow, setLink }) => {
   // state for api data
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   // url with no-cors mode
   const url =
     'https://cors-anywhere.herokuapp.com/' +
     'https://elsan.void.fr/fr/jsonapi/node/elsan_praticien';
   // fetch data from api
   const fetchData = async (url) => {
+    setIsLoading(true);
     const res = await fetch(url);
     const { data } = await res.json();
     setItems(data);
+    setIsLoading(false);
   };
   // call fetchData when the component is mounted with lifecycle method useEffect
   useEffect(() => {
     fetchData(url);
     // eslint-disable-next-line
   }, []);
+  if (isLoading) {
+    return (
+      <div className='flex h-96 justify-center items-center'>
+        <h1 className='sm:text-5xl text-xl'>Loading...</h1>
+      </div>
+    );
+  }
   return (
     <div className='border-t-4 border-t-primary border-solid shadow-md divide-y divide-gray-300'>
       {/* map through the items state */}
